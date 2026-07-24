cbab70ffbc2360d97ed64c08d6e5ec19903cdc94
sumit verma <sumit.verma.gbbic@gmail.com>
2026-07-24 23:27:13 +0530

    Merga and Quick

---
+++ sorting/quick.py
@@ -0,0 +1,28 @@
+def partition(arr, si, ei):
+    pivot = arr[ei]      # Last element as pivot
+    i = si - 1
+
+    for j in range(si, ei):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    # Place pivot at correct position
+    arr[i + 1], arr[ei] = arr[ei], arr[i + 1]
+
+    return i + 1
+
+
+def quick_sort(arr, si, ei):
+    if si < ei:
+        p = partition(arr, si, ei)
+
+        quick_sort(arr, si, p - 1)
+        quick_sort(arr, p + 1, ei)
+
+
+arr = [23, 43, 12, 5, 76, 23]
+
+quick_sort(arr, 0, len(arr) - 1)
+
+print(arr)
